ade154a59f406eb54bafd1b8696fea73a7c9a51c
toolemus <226151689+toolemus@users.noreply.github.com>
2025-10-07 06:13:42 -0500

    Update panthy WAN 2.2.sh

--- panthy WAN 2.2.sh
+++ panthy WAN 2.2.sh
@@ -161,7 +161,7 @@ function provisioning_get_pip_packages() {
 
 # We must be at release tag v0.3.49 or greater for fp8 support
 provisioning_update_comfyui() {
-    required_tag="v0.3.49"
+    required_tag="v0.3.60"
     cd ${COMFYUI_DIR}
     git fetch --all --tags
     current_commit=$(git rev-parse HEAD)
@@ -270,6 +270,7 @@ if [[ ! -f /.noprovisioning ]]; then
     provisioning_start
 
 fi
+
 
 
 
