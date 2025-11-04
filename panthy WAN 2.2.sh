4e340f61daf80f12f300ad705a5c2982bae6c563
toolemus <226151689+toolemus@users.noreply.github.com>
2025-11-04 04:13:24 -0600

    Update panthy WAN 2.2.sh

--- panthy WAN 2.2.sh
+++ panthy WAN 2.2.sh
@@ -55,7 +55,6 @@ DIFFUSION_MODELS=(
     #"https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors"  
     "https://huggingface.co/lightx2v/Wan2.2-Distill-Models/resolve/main/wan2.2_i2v_A14b_low_noise_lightx2v_4step.safetensors"
     "https://huggingface.co/lightx2v/Wan2.2-Distill-Models/resolve/main/wan2.2_i2v_A14b_high_noise_lightx2v_4step_1030.safetensors"
-    
 )
 
 CLIP_MODELS=(
@@ -275,6 +274,7 @@ if [[ ! -f /.noprovisioning ]]; then
     provisioning_start
 
 fi
+
 
 
 
